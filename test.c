bed0695955cdd388faee84eb4ddbad2d2bd3ddce
yuki <chenjf2020@mail.sustech.edu.cn>
2022-09-28 14:15:09 +0800

    add lex

---
+++ test.c
@@ -0,0 +1,50 @@
+int test(int a, char b) {
+    return a * b; 
+}
+
+int main()
+{
+    int a = 0;
+    int BBA = a_;
+    int _;
+    const int avc = 114514.1919810;
+    int 3a;
+    if (a0 == 0) _ = b0;
+    else _= -b0;
+    while(b != 0) {b1 = b0 - b2;}
+    char c = '\'';
+    const char * str = "asfdasdf";
+    const char*const str2 = "asfdasdf";
+    str2 = "sadvasacvasd";
+    str = "aaaaaa";
+    const chars asf = "ada";
+    a = 123;
+    const double b = 1919.810;
+    if (a == 0 && b == 3) {
+        printf("q\"\"wq\n");
+    }
+
+    const int vasdvsaf = 1231;
+
+    int q2 = 0x00;
+    const int q3 = 0b0011;
+    const int q4 = 0x7FFF0000LL;
+    const unsigned char ads = '1';
+    a++;
+
+    ++q2;
+    q2=q2+123123UL;
+    signed int *const ptr = (int *) &q3;
+
+    *ptr = 0x121262;
+
+    int app[100];
+
+    app[1] = 123;
+    for (int i=0;i<123;i++) {
+        app[i] = 66 * i;
+        app[i] = test(i, i*i);
+    }
+    return 0;
+}
+
