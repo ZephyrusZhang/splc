c448f1dbf029992f1dc2d85714426021d08bb8c8
ZephyrusZhang <82928147+ZephyrusZhang@users.noreply.github.com>
2022-11-20 02:28:48 +0800

    test: test case run script

--- run.py
+++ run.py
@@ -2,6 +2,8 @@ import argparse
 import logging
 import os
 
+from os.path import isfile, join, splitext, basename
+
 
 class CustomFormatter(logging.Formatter):
 
@@ -59,21 +61,17 @@ def test(test_case_path: str, output_path: str, answer_path: str):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('-t', '--test-case', default=None, type=int)
+    parser.add_argument('-f', '--file-dir', type=str, required=True, help='spl file directory')
     opt = parser.parse_args()
 
-    os.system('make')
+    os.system('make -j 8')
 
-    if opt.test_case is None:
-        for i in range(1, 13):
-            num = f'0{i}' if i < 10 else i
-            test(f'test/test-phase-1/test_1_r{num}.spl',
-                f'test/test-phase-1/test_1_r{num}.out',
-                f'test/out/test_1_r{num}.out')
-    else:
-        num = f'0{opt.test_case}' if opt.test_case < 10 else opt.test_case
-        test(f'test/test-phase-1/test_1_r{num}.spl',
-                f'test/test-phase-1/test_1_r{num}.out',
-                f'test/out/test_1_r{num}.out')
+    files_path = [join(opt.file_dir, filr_or_dir) for filr_or_dir in os.listdir(opt.file_dir) if isfile(join(opt.file_dir, filr_or_dir))]
+    standard_output_dir = join(opt.file_dir, 'standard-out')
 
-    os.system('make clean')
+    for file_path in files_path:
+        file_name, _ = splitext(basename(file_path))
+        print(file_name)
+        test(test_case_path=file_path,
+             output_path=join(opt.file_dir, f'{file_name}.out'),
+             answer_path=join(standard_output_dir, f'{file_name}.out'))
