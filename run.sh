65f09e4a452dd2d7a01d28c8d538ae7b1263f9a8
ZephyrusZhang <2996362441@qq.com>
2023-01-02 11:47:30 +0800

    test: extra test

--- run.sh
+++ run.sh
@@ -9,7 +9,11 @@ bin/splc test/test_3_r08.spl
 bin/splc test/test_3_r09.spl
 bin/splc test/test_3_r10.spl
 
-bin/splc test-extra/
+bin/splc test-extra/test_3_b01.spl
+bin/splc test-extra/test_3_b02.spl
+bin/splc test-extra/test_3_b03.spl
+bin/splc test-extra/test_3_b04.spl
+bin/splc test-extra/test_struct.spl
 
 gcc -x c test-cpp/test_3_r01.c -o test-cpp/test_3_r01.out
 gcc -x c test-cpp/test_3_r02.c -o test-cpp/test_3_r02.out
@@ -21,3 +25,9 @@ gcc -x c test-cpp/test_3_r07.c -o test-cpp/test_3_r07.out
 gcc -x c test-cpp/test_3_r08.c -o test-cpp/test_3_r08.out
 gcc -x c test-cpp/test_3_r09.c -o test-cpp/test_3_r09.out
 gcc -x c test-cpp/test_3_r10.c -o test-cpp/test_3_r10.out
+
+gcc -x c test-cpp/test_3_b01.c -o test-cpp/test_3_b01.out
+gcc -x c test-cpp/test_3_b02.c -o test-cpp/test_3_b02.out
+gcc -x c test-cpp/test_3_b03.c -o test-cpp/test_3_b03.out
+gcc -x c test-cpp/test_3_b04.c -o test-cpp/test_3_b04.out
+gcc -x c test-cpp/test_struct.c -o test-cpp/test_struct.out
